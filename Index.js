1dd2a4234da87438bc843f239bf430661815fc3a
Viskto <webdev.vitor@gmail.com>
2022-09-10 10:26:03 -0300

    Add Html skeleton with book form

---
+++ Index.js
@@ -0,0 +1,17 @@
+const newBook = document.querySelector(".new_book")
+const myForm = document.querySelector("form")
+
+
+newBook.addEventListener("click", () =>{
+    myForm.classList.remove("hide");
+})
+
+let myLibrary = [];
+
+function Book(){
+
+}
+
+function addBookToLibrary(){
+
+}
