f2ef7698410eff8715a5390e4495b60313b4bc36
Viskto <webdev.vitor@gmail.com>
2022-09-10 10:57:11 -0300

    Add object constructor

--- Index.js
+++ Index.js
@@ -1,17 +1,34 @@
-const newBook = document.querySelector(".new_book")
-const myForm = document.querySelector("form")
-
-
-newBook.addEventListener("click", () =>{
-    myForm.classList.remove("hide");
-})
+const newBook = document.querySelector(".new_book");
+const myForm = document.querySelector("form");
+const submit = document.querySelector(".submit_button");
+const bTitle = document.querySelector("#book_title");
+const bAuthor = document.querySelector("#book_author");
+const bPages = document.querySelector("#number_of_pages");
+const rButtons = document.querySelectorAll("[type=radio]");
 
 let myLibrary = [];
 
-function Book(){
+newBook.addEventListener("click", () => {
+  myForm.classList.remove("hide");
+});
 
-}
+submit.addEventListener("click", (e) => {
+  let myRadio;
+  rButtons.forEach((button) => {
+    if (button.checked === true) {
+      myRadio = button.value;
+      console.log(myRadio);
+    }
+  });
+  myLibrary.push(new Book(bTitle.value, bAuthor.value, bPages.value, myRadio));
+  e.preventDefault();
+});
 
-function addBookToLibrary(){
-
+function Book(title, author, pages, haveread) {
+  (this.title = title), (this.author = author), (this.pages = pages);
+  if (haveread === "yes") {
+    this.haveread = "Already read";
+  } else {
+    this.haveread = "Haven't read yet";
+  }
 }
