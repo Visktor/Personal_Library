514307c7740a0e4e943399600af328925fbf8ec3
Viskto <webdev.vitor@gmail.com>
2022-09-10 16:14:06 -0300

    Add library UI skeleton

--- Index.js
+++ Index.js
@@ -5,7 +5,7 @@ const bTitle = document.querySelector("#book_title");
 const bAuthor = document.querySelector("#book_author");
 const bPages = document.querySelector("#number_of_pages");
 const rButtons = document.querySelectorAll("[type=radio]");
-const libUI = document.querySelector(".library")
+const libUI = document.querySelector(".library");
 
 let myLibrary = [];
 let currentBookIndex = -1;
@@ -25,7 +25,28 @@ submit.addEventListener("click", (e) => {
   myLibrary.push(new Book(bTitle.value, bAuthor.value, bPages.value, myRadio));
   e.preventDefault();
   currentBookIndex += 1;
-  
+  let nDiv = document.createElement("div");
+  nDiv.classList.add("book_div");
+  libUI.appendChild(nDiv);
+  for (let i = 1; i <= 4; i++) {
+    let nnDiv = document.createElement("div");
+    nnDiv.classList = `div${i}`;
+    switch (i) {
+      case 1:
+        nnDiv.textContent = `Title: ${myLibrary[currentBookIndex].title}`;
+        break;
+      case 2:
+        nnDiv.textContent = `Author: ${myLibrary[currentBookIndex].author}`;
+        break;
+      case 3:
+        nnDiv.textContent = `Number of Pages: ${myLibrary[currentBookIndex].pages}`;
+        break;
+      case 4:
+        nnDiv.textContent = myLibrary[currentBookIndex].haveread;
+        break;
+    }
+    nDiv.appendChild(nnDiv);
+  }
 });
 
 function Book(title, author, pages, haveread) {
@@ -36,4 +57,3 @@ function Book(title, author, pages, haveread) {
     this.haveread = "Haven't read yet";
   }
 }
-
