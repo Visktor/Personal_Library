64690653fa6e741336c80328df66722b5675ecca
Viskto <webdev.vitor@gmail.com>
2022-09-11 05:15:56 -0300

    Further organizes function

--- Index.js
+++ Index.js
@@ -16,23 +16,48 @@ newBook.addEventListener("click", () => {
 
 submit.addEventListener("click", (e) => {
   addToLibArray();
+  createLibDiv();
+  createDeleteButton();
+  createInfoDivs();
   e.preventDefault();
+});
+
+function createLibDiv() {
   currentBookIndex += 1;
-  let theObjIndex = currentBookIndex;
   let nDiv = document.createElement("div");
   nDiv.classList.add("book_div");
   nDiv.setAttribute("data_libIndex", currentBookIndex);
   libUI.appendChild(nDiv);
+}
+
+function addToLibArray() {
+  myLibrary.push(
+    new Book(bookTitle.value, bookAuthor.value, bookPages.value, checkRadio())
+  );
+}
+
+function checkRadio() {
+  radioButtons.forEach((button) => {
+    if (button.checked === true) {
+      return button.value;
+    }
+  });
+}
+
+function createDeleteButton() {
   let nButton = document.createElement("button");
   nButton.textContent = "X";
   nButton.classList.add("delete_button");
+  nDiv.appendChild(nButton);
   nButton.addEventListener("click", function () {
     let ourdiv = document.querySelector(
       `[data_libIndex="${currentBookIndex}"]`
     );
     ourdiv.remove();
   });
-  nDiv.appendChild(nButton);
+}
+
+function createInfoDivs() {
   for (let i = 1; i <= 4; i++) {
     let nnDiv = document.createElement("div");
     nnDiv.classList = `div${i}`;
@@ -48,17 +73,17 @@ submit.addEventListener("click", (e) => {
         break;
       case 4:
         nnDiv.addEventListener("click", () => {
-          myLibrary[theObjIndex].changeStatus();
-          if (myLibrary[theObjIndex].opt) {
+          myLibrary[currentBookIndex].changeStatus();
+          if (myLibrary[currentBookIndex].opt) {
             nnDiv.style.backgroundColor = "green";
           } else {
             nnDiv.style.backgroundColor = "red";
           }
-          nnDiv.textContent = myLibrary[theObjIndex].haveread;
+          nnDiv.textContent = myLibrary[currentBookIndex].haveread;
         });
 
         nnDiv.textContent = myLibrary[currentBookIndex].haveread;
-        if (myLibrary[theObjIndex].opt) {
+        if (myLibrary[currentBookIndex].opt) {
           nnDiv.style.backgroundColor = "green";
         } else {
           nnDiv.style.backgroundColor = "red";
@@ -67,23 +92,6 @@ submit.addEventListener("click", (e) => {
     }
     nDiv.appendChild(nnDiv);
   }
-});
-
-
-
-
-function addToLibArray() {
-  myLibrary.push(
-    new Book(bookTitle.value, bookAuthor.value, bookPages.value, checkRadio())
-  );
-}
-
-function checkRadio() {
-  radioButtons.forEach((button) => {
-    if (button.checked === true) {
-      return button.value;
-    }
-  });
 }
 
 function Book(title, author, pages, haveread) {
